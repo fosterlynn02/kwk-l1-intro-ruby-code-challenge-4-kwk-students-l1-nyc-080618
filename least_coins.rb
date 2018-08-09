500a2076728b9575aecfb9ab56865481eded8bbe
fosterlynn02 <fosterlynnboulin@gmail.com>
2018-08-09 13:51:12 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -3,7 +3,8 @@
 def least_coins(cents)
   
   least_coins = 29 cents 
- least_coin_hash.each do [["quaters","25 cents"],["dimes","10 cents"], ["Nichels", "5 cents"], []
+ least_coin_hash.each do [["quaters","25 cents"],["dimes","10 cents"], ["Nickels", "5 cents"], ["pennies", "1 cent"]
+ 
 
 #Code your answer here!
 end
